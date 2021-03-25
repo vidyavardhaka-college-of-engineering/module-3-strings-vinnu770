96e965cc8026b8f0e9e30dea213592ca1649b023
vinnu770 <vinnureddy05@gmail.com>
2021-03-25 10:23:27 +0000

    done

--- Program-4.c
+++ Program-4.c
@@ -9,14 +9,13 @@
 //The length of string = 13
 
 #include <stdio.h>
-int main()
-{
- 
-    // Declare the required variables
+int main() {
+    char str[100];
+    int i;
     printf("Enter a string: ");
     scanf("%s",str);
     for(i=0; str[i]!='\0'; ++i);
-       printf("The length of string = );
-       //Complete the missing code in result printing statement
+    printf("The length of string = %d",i);
     return 0;
 }
+
