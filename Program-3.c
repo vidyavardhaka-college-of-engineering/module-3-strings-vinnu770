7713697728e25bfc29fdc28234c7f76717a6b2e0
vinnu770 <vinnureddy05@gmail.com>
2021-03-25 10:14:30 +0000

    done

--- Program-3.c
+++ Program-3.c
@@ -18,16 +18,16 @@ int main() {
 	scanf("%s", a);
 	printf("Enter the second string : ");
 	scanf("%s", b);
-	for ( )                                                // Complete the code in for
+	for (i=0;i<20;i++)                                                // Complete the code in for
            { 
-		c[i] = ;                                              //Complete the statement
+		c[i] = a[i] ;                                              //Complete the statement
 	}
-	for ( )                                                 // Complete the code in for
+	for (i=0;i<20;i++ )                                                 // Complete the code in for
            { 
-		c[i] = ;                                              //Complete the statement
+		c[i] =c[i]+b[i] ;                                              //Complete the statement
 		i++;
 	}
-	c[i]= ;                                               //Complete the statement
-	printf("The concatenated string = %s", c);
+	c[i]=a[i]+b[i] ;                                               //Complete the statement
+	printf("The concatenated string = %s%s", a,b);
 	return 0;
 }
